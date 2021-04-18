e42ad2a3b6072c02473990ff6d20f037cd2935d1
Naryuko <55742167+Naryuko@users.noreply.github.com>
2021-04-18 21:03:51 +0900

    Create 큰 수 만들기.swift

---
+++ programmers/큰 수 만들기.swift
@@ -0,0 +1,40 @@
+// subArray를 만드는 것으 시간이 매우 오래걸리므로 하지말자.
+import Foundation
+
+func solution(_ number:String, _ k:Int) -> String {
+    var ret: [Character] = []
+    let list = Array(number)
+    let count = list.count
+    var remain = count - k
+    var start = 0
+    
+    while remain > 0 {
+//        print("start: ",start," end: ",count-remain, " ret: ",ret)
+//        let temp = Array(list[start...count - remain])
+        let (index, val) = findMax(a: list, start: start, end: count - remain)
+        start = index + 1
+        ret.append(val)
+        remain -= 1
+    }
+    
+    return String(ret)
+}
+
+func findMax (a: [Character], start: Int, end: Int) -> (Int, Character) {
+    var maxx: Character = "-"
+    var index = 0
+    
+    for i in start...end {
+        if a[i] == "9" {
+            maxx = "9"
+            index = i
+            break
+        }
+        if maxx < a[i] {
+            maxx = a[i]
+            index = i
+        }
+    }
+    
+    return (index, maxx)
+}
